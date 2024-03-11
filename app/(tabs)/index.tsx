8c230e02ba72db1eff371b8312c8dad3a5c8787f
Yuhyeon0516 <rladbgus0516@naver.com>
2024-03-11 13:29:32 +0900

    set detail page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 export default function Page() {
     return (
         <View>
-            <Text>Page</Text>
+            <Link href={"/(modals)/login"}>Login</Link>
+            <Link href={"/(modals)/booking"}>Booking</Link>
+            <Link href={"/detail/123"}>Detail</Link>
         </View>
     );
 }
